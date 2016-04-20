60dcf0ba410d405658cac1949d6d3aed21da2c98
SirheiNichyparuk <Siarhei_Nichyparuk@epam.com>
2016-04-20 13:51:59 +0300

    Added files via upload

---
+++ Third.java
@@ -0,0 +1,18 @@
+
+public class Third {
+	
+	
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 8;
+		int p;
+		int s;
+		
+		p = a+b;
+		System.out.println("Perimeter of a triangle = " + p);
+		s = a*b/2;
+		System.out.println("Area of a triangle = " + s);
+	
+	}
+
+}
