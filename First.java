5334ee6e5f78233406b337fafcef119e11b53b00
SirheiNichyparuk <Siarhei_Nichyparuk@epam.com>
2016-04-20 12:00:05 +0300

    Added files via upload

--- First.java
+++ First.java
@@ -1,30 +1,38 @@
+package MyPackage;
+
 import java.util.Scanner;
 
 public class First {
 	   static int number = 0;
+	   int a,b,c,d;
+
 
 	public static void main(String[] args) {
 		 Scanner sc = new Scanner(System.in);
-		 System.out.println("Введите четырехзначное число:");
+		 System.out.println("Enter four-digit number:");
 	     if(sc.hasNextInt()){
 	    	int number = sc.nextInt();
-	    	System.out.println("Ваше введенное число: "+ number);
+	    	System.out.println("Your entered number: "+ number);
 	    	if( number >9999){
-	    	 System.out.println("Вы ввели слишком большое число, введите четырехзначное число!!!");
+	    	 System.out.println("You have entered too big number, enter four-digit number!!!");
 	    	}
 	    		if(number <1000){
-	    	 System.out.println("Вы ввели слишком малое число, введите четырехзначное число!!!");
+	    	 System.out.println("You have entered very small number, enter four-digit number!!!");
 
 	    		}
 	     }	
 	     else{
-	    	 	System.out.println("Неверный ввод, введите четырехзначное число!!!");
+	    	 	System.out.println("Invalid input, enter four-digit number!!!");
 	     } 
-	     if (number / 1 % 10 + number / 10 % 10 == number / 100 % 10 + number / 1000 % 10) {
-             System.out.println(true);
-         } else {
-             System.out.println(false);
-         } 
+	     
+	     if(){
+	    	 
+	     }
+	     /*if (number / 1 % 10 + number / 10 % 10 == number / 100 % 10 + number / 1000 % 10) {
+          System.out.println(true);
+      } else {
+          System.out.println(false);
+      } */
 	     
 	     sc.close();
 	}
